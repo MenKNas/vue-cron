7a9e64762f3892c223ac7ee40d9f59deee3456e9
Menios Nasies <mnasies@velti.com>
2019-06-20 17:57:46 +0300

    changes on the app,ce15cd.js file

--- src/index.vue
+++ src/index.vue
@@ -58,7 +58,7 @@
                     <el-row>
                         <el-radio class="long" v-model="second.cronEvery" label="3">{{text.Seconds.specific}}
                             <el-select size="small" multiple v-model="second.specificSpecific">
-                                <el-option v-for="val in 60" :key="$index" :value="val-1">{{val-1}}</el-option>
+                                <el-option placeholder="Choose Seconds" v-for="val in 60" :key="$index" :value="val-1">{{val-1}}</el-option>
                             </el-select>
                         </el-radio>
                     </el-row>
